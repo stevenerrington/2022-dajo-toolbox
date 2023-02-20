893d7d42a135c3d05830a989baf37b1564a8a297
Steven Errington <steven.errington@outlook.com>
2023-02-19 18:37:33 -0600

    Added key functions from other projects

--- neural/spk_alignTrials.m
+++ neural/spk_alignTrials.m
@@ -1,4 +1,4 @@
-function [SDF] = spk_alignTrials(trialEventTimes, spkTimes, timeWin)
+function [sdf, spk, raster] = spk_alignTrials(trialEventTimes, spkTimes, timeWin)
 
 names = fieldnames( spkTimes );
 subStr = 'DSP';
@@ -14,25 +14,39 @@ for DSPidx = 1:length(DSPnames)
     eventNames = fieldnames(trialEventTimes);
     eventNames = eventNames(1:length(eventNames)-3);
     
+    alignedSDF = {}; alignedSPK = {}; alignedRaster = {};
+    
+    
     for alignIdx = 1:length(eventNames)
         alignTimes = round(trialEventTimes.(eventNames{alignIdx})(:));
         
         alignedSDF_event = nan(length(alignTimes),range(timeWin)+1);
+        raster_temp = zeros(length(alignTimes),range(timeWin)+1);
         
-        for ii = 1:length(alignTimes)
-            if isnan(alignTimes(ii)) | alignTimes(ii) == 0
-                continue
+        spk_aligntemp = {};
+        for trl_i = 1:length(alignTimes)
+            if isnan(alignTimes(trl_i)) | alignTimes(trl_i) == 0
+                spk_aligntemp{trl_i,1} = [];
             else
-                alignedSDF_event(ii,:) = SessionSDF(alignTimes(ii)+timeWin(1):alignTimes(ii)+timeWin(end));
+                alignedSDF_event(trl_i,:) = SessionSDF(alignTimes(trl_i)+timeWin(1):alignTimes(trl_i)+timeWin(end));
+                spk_aligntemp{trl_i,1} = intersect(spkTimes.(DSPlabel),alignTimes(trl_i)+[timeWin(1):timeWin(2)])-...
+                    alignTimes(trl_i);
+                
+                trl_spkTimes = spk_aligntemp{trl_i,1}+abs(min(timeWin(1)))+1;
+                raster_temp(trl_i,trl_spkTimes) = 1;
             end
         end
         
         alignedSDF{alignIdx} = alignedSDF_event;
+        alignedSPK{alignIdx} = spk_aligntemp;
+        alignedRaster{alignIdx} = raster_temp;
         
     end
     
     for alignIdx = 1:length(eventNames)
-        SDF.(DSPlabel).(eventNames{alignIdx}) = alignedSDF{alignIdx};
+        sdf.(DSPlabel).(eventNames{alignIdx}) = alignedSDF{alignIdx};
+        spk.(DSPlabel).(eventNames{alignIdx}) = alignedSPK{alignIdx};
+        raster.(DSPlabel).(eventNames{alignIdx}) = alignedRaster{alignIdx};
     end
     
     clear alignedSDF aligned_spkTimes
--- utils/load_datamap.m
+++ utils/load_datamap.m
@@ -1,5 +1,5 @@
-function dajo_datamap = load_datamap(dirs)
-    file_location = fullfile(dirs.nest, dirs.toolbox, 'data','2021-dajo-datamap.mat');
+function dajo_datamap = load_datamap(input_dir)
+    file_location = fullfile(input_dir,'2021-dajo-datamap.mat');
     load(file_location);
     clear file_location
 end
